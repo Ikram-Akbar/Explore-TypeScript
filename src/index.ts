75df63d0da743f77d950a5fb53d2828eac883d0c
Ikram Akbar <ikramakbar71@gmail.com>
2022-09-19 17:18:16 +0600

    commit

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+console.log("hello world ");
+// create a variable with ts
+let num: string = "this is a variable with typescript..";
+
+let age: number = 25;
+if (age < 18) {
+    age += 10;
+    console.log()
+};
+
+let price: number = 120;
+let pc: string = "dell";
+let is_your: boolean = false;
+let level;
+
+const fun = (doc: any) => {
+    console.log(doc);
+}
+
+// Array in ts
+
+const newNum: number[] = [12, 2, 3];
+newNum.forEach(n => n.toFixed());
+
+
+
+// tuples
+
+let user: [number, string, boolean] = [1, "dell", false];
+
+
+// enum
+
+enum size { Small = 5, Medium, Large };
+let mySize: size = size.Large;
+console.log(mySize);
+// for better code output 
+const enum size1 { Small = 5, Medium, Large };
+let mySize1: size1 = size1.Small;
+console.log(mySize1);
+
+
+// function in ts
+
+function calculatesTex(income: number, taxYear: number): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+
+}
+calculatesTex(11_000, 2022,)
+
+
+
+
+// create an another function for find out the sum
+
+const multi = (para1: number, para2: number) => {
+    if (para1 < 0) {
+        return 0;
+    }
+    else {
+        return para1 * para2;
+    }
+}
+
